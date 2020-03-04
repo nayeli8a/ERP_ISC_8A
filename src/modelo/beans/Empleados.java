9f44b3e150874d826966624ca9e5dd1afea4952b
Cocom <fcocom337@accitesz.com>
2020-03-04 14:34:31 -0600

    Objeto de la tabla Empleados

---
+++ src/modelo/beans/Empleados.java
@@ -0,0 +1,173 @@
+package modelo.beans;
+
+import java.io.InputStream;
+import java.sql.Date;
+
+public class Empleados{
+
+  private int idEmpleado;
+  private String nombre;
+  private String apaterno;
+  private String amaterno;
+  private char  sexo;
+  private Date fechaContratacion;
+  private Date fechaNacimiento;
+  private float salario;
+  private String nss;
+  private String estadoCivil;
+  private int diasVacaciones;
+  private int diasPermiso;
+  private InputStream fotografia;
+  private String direccion;
+  private String colonia;
+  private String codigoPostal;
+  private String escolaridad;
+  private float porcentajeComision;
+  private char estatus;
+  private int idDepartamento;
+  private int idPuesto;
+  private int idCiudad;
+  private int idSucursal;
+  
+public int getIdEmpleado() {
+	return idEmpleado;
+}
+public void setIdEmpleado(int idEmpleado) {
+	this.idEmpleado = idEmpleado;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApaterno() {
+	return apaterno;
+}
+public void setApaterno(String apaterno) {
+	this.apaterno = apaterno;
+}
+public String getAmaterno() {
+	return amaterno;
+}
+public void setAmaterno(String amaterno) {
+	this.amaterno = amaterno;
+}
+public char getSexo() {
+	return sexo;
+}
+public void setSexo(char sexo) {
+	this.sexo = sexo;
+}
+public Date getFechaContratacion() {
+	return fechaContratacion;
+}
+public void setFechaContratacion(Date fechaContratacion) {
+	this.fechaContratacion = fechaContratacion;
+}
+public Date getFechaNacimiento() {
+	return fechaNacimiento;
+}
+public void setFechaNacimiento(Date fechaNacimiento) {
+	this.fechaNacimiento = fechaNacimiento;
+}
+public float getSalario() {
+	return salario;
+}
+public void setSalario(float salario) {
+	this.salario = salario;
+}
+public String getNss() {
+	return nss;
+}
+public void setNss(String nss) {
+	this.nss = nss;
+}
+public String getEstadoCivil() {
+	return estadoCivil;
+}
+public void setEstadoCivil(String estadoCivil) {
+	this.estadoCivil = estadoCivil;
+}
+public int getDiasVacaciones() {
+	return diasVacaciones;
+}
+public void setDiasVacaciones(int diasVacaciones) {
+	this.diasVacaciones = diasVacaciones;
+}
+public int getDiasPermiso() {
+	return diasPermiso;
+}
+public void setDiasPermiso(int diasPermiso) {
+	this.diasPermiso = diasPermiso;
+}
+public InputStream getFotografia() {
+	return fotografia;
+}
+public void setFotografia(InputStream fotografia) {
+	this.fotografia = fotografia;
+}
+public String getDireccion() {
+	return direccion;
+}
+public void setDireccion(String direccion) {
+	this.direccion = direccion;
+}
+public String getColonia() {
+	return colonia;
+}
+public void setColonia(String colonia) {
+	this.colonia = colonia;
+}
+public String getCodigoPostal() {
+	return codigoPostal;
+}
+public void setCodigoPostal(String codigoPostal) {
+	this.codigoPostal = codigoPostal;
+}
+public String getEscolaridad() {
+	return escolaridad;
+}
+public void setEscolaridad(String escolaridad) {
+	this.escolaridad = escolaridad;
+}
+public float getPorcentajeComision() {
+	return porcentajeComision;
+}
+public void setPorcentajeComision(float porcentajeComision) {
+	this.porcentajeComision = porcentajeComision;
+}
+public char getEstatus() {
+	return estatus;
+}
+public void setEstatus(char estatus) {
+	this.estatus = estatus;
+}
+public int getIdDepartamento() {
+	return idDepartamento;
+}
+public void setIdDepartamento(int idDepartamento) {
+	this.idDepartamento = idDepartamento;
+}
+public int getIdPuesto() {
+	return idPuesto;
+}
+public void setIdPuesto(int idPuesto) {
+	this.idPuesto = idPuesto;
+}
+public int getIdCiudad() {
+	return idCiudad;
+}
+public void setIdCiudad(int idCiudad) {
+	this.idCiudad = idCiudad;
+}
+public int getIdSucursal() {
+	return idSucursal;
+}
+public void setIdSucursal(int idSucursal) {
+	this.idSucursal = idSucursal;
+}
+
+  
+  
+}
