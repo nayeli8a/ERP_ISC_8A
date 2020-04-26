d671ae730277878b28ec176043f6e4f727b62d65
Cocom <fcocom337@accitesz.com>
2020-04-26 11:01:14 -0500

    Create DocumentacionEmpleado.java

---
+++ src/modelo/beans/DocumentacionEmpleado.java
@@ -0,0 +1,55 @@
+package modelo.beans;
+
+import java.io.InputStream;
+import java.sql.Date;
+
+
+public class DocumentacionEmpleado{
+
+  private int idDocumento;
+  private String nombreDocumento;
+  private Date fechaEntrega;
+  private InputStream documento;
+  private int idEmpleado;
+  private String nss;
+  
+public String getNss() {
+	return nss;
+}
+public void setNss(String nss) {
+	this.nss = nss;
+}
+public int getIdDocumento() {
+	return idDocumento;
+}
+public void setIdDocumento(int idDocumento) {
+	this.idDocumento = idDocumento;
+}
+public String getNombreDocumento() {
+	return nombreDocumento;
+}
+public void setNombreDocumento(String nombreDocumento) {
+	this.nombreDocumento = nombreDocumento;
+}
+public Date getFechaEntrega() {
+	return fechaEntrega;
+}
+public void setFechaEntrega(Date fechaEntrega) {
+	this.fechaEntrega = fechaEntrega;
+}
+public InputStream getDocumento() {
+	return documento;
+}
+public void setDocumento(InputStream documento) {
+	this.documento = documento;
+}
+public int getIdEmpleado() {
+	return idEmpleado;
+}
+public void setIdEmpleado(int idEmpleado) {
+	this.idEmpleado = idEmpleado;
+}
+  
+  
+  
+}
